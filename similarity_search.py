1b8e5a17736a5e86b646a65feb4504f5956e106c
Dennis Ritter <dritter@beuth-hochschule.de>
2022-06-01 13:38:53 +0200

    add get_meshes_from_images, remouve unused

--- similarity_search.py
+++ similarity_search.py
@@ -1,20 +1,42 @@
 import os
-import random
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import faiss
 import torch
-import torchvision
-from transformers import ViTFeatureExtractor, ViTModel, FeatureExtractionPipeline
+from transformers import ViTFeatureExtractor, ViTModel
 from PIL import Image
-from tqdm import tqdm
-from sklearn.cluster import KMeans
-import cv2
-import json
-import pickle
 
-def get_neighbours(ims_q_fn: list[str], k: int=5, index_name:str ='vit_in21k_shapenet'):
+
+
+def get_meshes_from_images(ims_q_fn: list[str], k: int=5, index_name:str ='vit_in21k_shapenet'):
+    """ Returns meshes for given query images. The number of items that is returned varies as
+        nearest neighbour images may belong to the same mesh.
+    
+        Args:
+            ims_fn (list[str]): A list of filenames of query images.
+            k (int): The number of neighbours to find.
+            index_name (str): The index_name specifies the index/method that is used for retrieval. 
+    """
+    if index_name == 'vit_in21k_shapenet':
+        ims_fn = np.load(f'./data/{index_name}/imfn_{index_name}.npy')
+        distances, indices = get_neighbours(ims_q_fn, k, index_name)
+        
+        # Get neighbour indices for each query image
+        mesh_ids_for_qs = []
+        for ims_q_i, neighbours_i in enumerate(indices):
+            mesh_ids_for_q = []
+            for im_i in neighbours_i:
+                im_fn = ims_fn[im_i]
+                shapenet_id = im_fn.split('-')[-2] 
+                mesh_ids_for_q.append(shapenet_id)
+            # remove duplicates
+            mesh_ids_for_qs.append(list(set(mesh_ids_for_q)))
+        print(mesh_ids_for_qs)
+    else:
+        raise ValueError(f'Argument: index_name={index_name} is not a valid feature.')
+
+
+
+def get_neighbours(ims_q_fn: list[str], k: int, index_name:str ='vit_in21k_shapenet'):
     """ Returns list of tuples of distances and indices of the k nearest neighbours 
         for each given image filename from a search index of specified features.
 
@@ -53,5 +75,6 @@ def get_neighbours_vit_in21k(ims_q_fn: list[str], k: int):
     with torch.no_grad():
         outputs = model(**inputs).pooler_output
     outputs = np.array(outputs)
+
     distances, indices = index.search(outputs, k)
     return distances, indices
